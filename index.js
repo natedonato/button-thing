437825b5a34f81ce1fbe143da4e4d5100d42eb15
natedonato <natedonato@gmail.com>
2019-12-01 14:28:36 -0800

    moving button

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  let w = window.innerWidth;
+  let h = window.innerHeight; 
+  let speed = 0.2;
   const button = document.querySelector("button");
-  const div = document.querySelector("div");
-  // const buffer = 50;
+  
+  const div = document.querySelector("#buttonContainer");
+  const modal = document.querySelector(".Modal");
+
+
+  function centerButton(){
+  div.style.transform = `translate(${w / 2 - 50}px, ${h/2-50}px)`;
+  }
+  centerButton();
+  
+  function resizeWindow() {
+    h = window.innerHeight;
+    w = window.innerWidth;
+    centerButton();
+  }
+
+  function closeModal(){
+    modal.style.display = "none";
+    centerButton();
+  }
+
+
+  window.onresize = resizeWindow;
+
 
   div.addEventListener("mouseenter", e => {
-
-    div.style.transform = `translate(${Math.random(1)*1000}px, ${Math.random(1) * 1000}px)`;
+    if(speed < 2){
+      speed += 0.05;
+      div.style.transition = `cubic-bezier(0.4, 0, 0.2, 1) ${speed}s`;
+    }
+    div.style.transform = `translate(${Math.random(1)*(w-100)}px, ${Math.random(1) * (h-100)}px)`;
   });
 
   document.addEventListener("mousemove", e => {
@@ -19,6 +47,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   button.addEventListener("click", e => {
     e.preventDefault();
-    alert("YOU GOT ME");
+    speed = 0.2;
+    modal.style.display = 'flex';
   });
+
+  
+
 });
